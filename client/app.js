32dd1167c42b5f71f4c25f21a78bca6cf66283e8
joshWyatt <joshuawyatt123@gmail.com>
2014-07-11 09:07:52 -0700

    Begin implementing example bar chart with nvd3

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('Trrnts', []);
+var app = angular.module('Trrnts', ['nvd3ChartDirectives']);
 angular.module('trrnts', ['trrnts.magnetLink', 'trrnts.services', 'ui.router'])
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -7,4 +7,14 @@ angular.module('trrnts', ['trrnts.magnetLink', 'trrnts.services', 'ui.router'])
       url: '/',
       templateUrl: 'magnetlink/magnetLink.html'
     })
-});
+})
+
+//Below is practice setup for nvd3ChartDirectives, it is not ideal or elegant
+.controller('Nvd3Controller', ['$scope', function($scope){
+  $scope.exampleData = [
+    {
+      'key': "Series 1",
+      'values': [[ [ 1025409600000 , 0] , [ 1028088000000 , -6.3382185140371] , [ 1030766400000 , -5.9507873460847] , [ 1033358400000 , -11.569146943813] , [ 1036040400000 , -5.4767332317425] , [ 1038632400000 , 0.50794682203014] , [ 1041310800000 , -5.5310285460542] , [ 1043989200000 , -5.7838296963382] , [ 1046408400000 , -7.3249341615649] , [ 1049086800000 , -6.7078630712489] , [ 1051675200000 , 0.44227126150934] , [ 1054353600000 , 7.2481659343222] , [ 1056945600000 , 9.2512381306992] , [ 1059624000000 , 11.341210982529] , [ 1062302400000 , 14.734820409020] , [ 1064894400000 , 12.387148007542] , [ 1067576400000 , 18.436471461827] , [ 1070168400000 , 19.830742266977] , [ 1072846800000 , 22.643205829887] , [ 1075525200000 , 26.743156781239] , [ 1078030800000 , 29.597478802228] , [ 1080709200000 , 30.831697585341] , [ 1083297600000 , 28.054068024708] ]]
+    }
+  ]
+}]);
