c72d01523805b74324ab1d54f1e8f6fd2bbe40d6
joshWyatt <joshuawyatt123@gmail.com>
2014-07-14 12:12:25 -0700

    Remove timestamp from nodes

--- workers/crawler.js
+++ workers/crawler.js
@@ -9,57 +9,137 @@ var Crawler = function () {
   // Addresses as keys, since we need constant time insert operations and unique
   // entries (inserts every node only once).
   // We need a few "bootstrap nodes" as entry points for getting started.
-  this.nodes = {
-    'router.bittorrent.com:6881': timestamp,
-    'router.utorrent.com:6881': timestamp,
-    'dht.transmissionbt.com:6881': timestamp
-  };
+  // this.nodes = {
+  //   'router.bittorrent.com:6881': timestamp,
+  //   'router.utorrent.com:6881': timestamp,
+  //   'dht.transmissionbt.com:6881': timestamp
+  // };
+  this.nodes = [
+    'router.bittorrent.com:6881',
+    'router.utorrent.com:6881',
+    'dht.transmissionbt.com:6881'
+  ];
   this.peers = {};
 };
 
-// Recursively crawls the BitTorrent DHT protocol using an instance of the DHT
-// class, which is a property of the instance of the crawler.
+
+
+//if there are peers
+
+
+Crawler.prototype.catalogPeers = function(resp) {
+  _.each(resp.peers, function (peer) {
+    this.peers[peer] = _.now();
+
+    //add peers to redis set
+    redis.SADD('peer', peer);
+
+    //store each peer in a sorted set for its magnet. We will score each magnet by
+    //seeing how many peers there are for the magnet in the last X minutes
+    redis.ZADD('magnets:' + infoHash + ':peers', _.now(), peer);
+    // Store all peers to the geoQueue       
+    // this.pushPeersToGeoQueue(resp.peers, infoHash);    
+  }, this);
+};
+
+Crawler.prototype.crawlPeer = function (tStamp, peer){
+  this.dht.getPeers(infoHash, peer, function (err, resp) {
+    var numberOfPeersThisCrawl = _.keys(resp.peers).length;
+
+
+    if(numberOfPeersThisCrawl > 0) {
+      this.catalogPeers(resp);
+    }
+
+
+  }.bind(this));
+
+};
+
+Crawler.prototype.crawlNode = function (tStamp, node) {
+  this.dht.getPeers(infoHash, node, function (err, resp) {
+    var numberOfPeersThisCrawl = _.keys(this.peers).length;
+    console.log('crawlNode numberOfPeersThisCrawl -----------------------------------');
+
+    if(numberOfPeersThisCrawl > 0) {
+      console.log('MONEY ----------------------------------- !!!!!!!!');
+    }
+    _.each(resp.nodes, function (node) {
+
+      this.nodes[node] = _.now();
+      //add nodes to redis set
+      // redis.SADD('node', node);
+    }, this);
+  });
+};
+
 Crawler.prototype.crawl = function (infoHash) {
   var numberOfNodes = _.keys(this.nodes).length;
   var numberOfPeers = _.keys(this.peers).length;
 
-  _.each(this.nodes, function (tStamp, node) {
-    // console.log('----------------------------------- INSIDE CRAWL');
-    this.dht.getPeers(infoHash, node, function (err, resp) {
+  if(numberOfPeers > 0) {
+    console.log('crawl inside if -----------------------------------');
+    _.each(this.peers, this.crawlPeer, this);
+  } else {
+    console.log('crawl inside else -----------------------------------');
+    // _.each(this.nodes, this.crawlNode, this);
+    console.log(this.nodes);
+  }
 
-      _.each(resp.nodes, function (node) {
 
-        this.nodes[node] = _.now();
-        //add nodes to redis set
-        redis.SADD('node', node);
-      }, this);
+  // _.each(this.nodes, function (tStamp, node) {
+  //   // console.log('----------------------------------- INSIDE CRAWL');
+  // }, this);
 
-      _.each(resp.peers, function (peer) {
-        this.peers[peer] = _.now();
-
-        //add peers to redis set
-        redis.SADD('peer', peer);
-
-        //store each peer in a sorted set for its magnet. We will score each magnet by
-        //seeing how many peers there are for the magnet in the last X minutes
-        redis.ZADD('magnets:' + infoHash + ':peers', _.now(), peer);
-        // redis.ZREVRANGE('magnets:' + infoHash + ':peers', 0, 0, 'withscores', function(err, resp) {
-        //   console.log('----------------------------------- ' + resp);
-        // });                      
-      }, this);
-
-      // Store all peers to the geoQueue       
-      // this.pushPeersToGeoQueue(resp.peers, infoHash);    
-    }.bind(this));
-  }, this);
-
-  setTimeout(function () {
-    this.crawl(infoHash);
-  }.bind(this), 100);
+  this.crawl(infoHash);
 
   console.log(numberOfNodes + ' nodes');
   console.log(numberOfPeers + ' peers');
 };
+
+// Recursively crawls the BitTorrent DHT protocol using an instance of the DHT
+// class, which is a property of the instance of the crawler.
+// Crawler.prototype.crawl = function (infoHash) {
+//   var numberOfNodes = _.keys(this.nodes).length;
+//   var numberOfPeers = _.keys(this.peers).length;
+
+//   _.each(this.nodes, function (tStamp, node) {
+//     // console.log('----------------------------------- INSIDE CRAWL');
+//     this.dht.getPeers(infoHash, node, function (err, resp) {
+
+//       _.each(resp.nodes, function (node) {
+
+//         this.nodes[node] = _.now();
+//         //add nodes to redis set
+//         redis.SADD('node', node);
+//       }, this);
+
+//       _.each(resp.peers, function (peer) {
+//         this.peers[peer] = _.now();
+
+//         //add peers to redis set
+//         redis.SADD('peer', peer);
+
+//         //store each peer in a sorted set for its magnet. We will score each magnet by
+//         //seeing how many peers there are for the magnet in the last X minutes
+//         redis.ZADD('magnets:' + infoHash + ':peers', _.now(), peer);
+//         // redis.ZREVRANGE('magnets:' + infoHash + ':peers', 0, 0, 'withscores', function(err, resp) {
+//         //   console.log('----------------------------------- ' + resp);
+//         // });                      
+//       }, this);
+
+//       // Store all peers to the geoQueue       
+//       // this.pushPeersToGeoQueue(resp.peers, infoHash);    
+//     }.bind(this));
+//   }, this);
+
+//   setTimeout(function () {
+//     this.crawl(infoHash);
+//   }.bind(this), 100);
+
+//   console.log(numberOfNodes + ' nodes');
+//   console.log(numberOfPeers + ' peers');
+// };
 
 Crawler.prototype.start = function (callback) {
   this.dht.start(callback);
